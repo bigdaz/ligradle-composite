8c4962a93667aee116a692fd89a9b7f7c28d74ad
Daz DeBoer <daz@gradle.com>
2017-02-11 13:10:38 -0700

    ligradle-jvm-0.1

---
+++ ligradle-jvm-0.1/src/main/java/org/sample/LiGradleJvmPlugin.java
@@ -0,0 +1,26 @@
+package org.sample;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.api.Action;
+import org.gradle.api.Task;
+
+import java.util.LinkedHashMap;
+
+public class LiGradleJvmPlugin implements Plugin<Project> {
+
+    public void apply(Project project) {
+        project.getPluginManager().apply(org.gradle.api.plugins.JavaPlugin.class);
+        project.getPluginManager().apply(org.gradle.plugins.ide.idea.IdeaPlugin.class);
+
+        project.getTasks().create("precompileAll", PreCompile.class);
+        project.getTasks().withType(JavaCompile.class, new Action<Task>() {
+            @Override
+            public void execute(Task task) {
+                task.dependsOn("precompileAll");
+            }
+        });
+    }
+
+}
