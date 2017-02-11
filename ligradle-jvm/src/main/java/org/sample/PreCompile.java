397915879ad26658ff738886e394d6b51006be16
Daz DeBoer <daz@gradle.com>
2017-02-11 13:15:13 -0700

    ligradle-jvm uses older version of self for building

---
+++ ligradle-jvm/src/main/java/org/sample/PreCompile.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+
+public class PreCompile extends DefaultTask {
+
+    @TaskAction
+    public void precompile() {
+        System.out.println("Precompiling...");
+    }
+}
